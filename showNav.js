c0fb77cf798689af8f7aff4b46de54d6e9450400
Alvin J. Alexander <stefan.barcelo@gmail.com>
2019-10-24 10:04:59 -0400

    adding smooth scrolling to <a> tags

--- showNav.js
+++ showNav.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $(window).scroll(function () {
       var scroll = $(window).scrollTop();
       if (scroll > 1) {
-        $("#nav").css("background", "rgba(7, 10, 47, .95)");
+        $("#nav").css("background", "rgba(7, 10, 47, .97)");
       }
       else {
         $("#nav").css("background", "rgba(7, 10, 47, 0)");
